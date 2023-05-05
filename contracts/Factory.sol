c0ad42e071e818f1277a9292f05b378a87d25dcb
Ultra-tech-code <hizick27@gmail.com>
2023-05-05 07:12:41 +0100

    pushed codebase

---
+++ contracts/Factory.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./TestContract.sol";
+
+contract Factory {
+
+    event NewContract(address indexed contractAddress);
+
+    /**
+     * @notice  . A function to Get bytecode of contract to be deployed
+     * @dev     . returns bytes [bytes of TestContract + constructor argument]
+     * @param   _owner  . An address[TestContract constructor arguments]
+     * @param   _name  . A string[TestContract constructor arguments]
+    */
+    function getContractBytecode(address _owner, string calldata  _name) public pure returns (bytes memory) {
+        bytes memory bytecode = type(TestContract).creationCode;
+
+        return abi.encodePacked(bytecode, abi.encode(_owner, _name));
+    }
+
+
+    /**
+     * @notice  . A function to Compute address of the contract to be deployed
+     * @dev     . returns address where the contract will deployed to if deployed with create2
+     * @param   salt: random bytes used to precompute an address
+    */
+    function getAddress(bytes32 salt, bytes memory bytecode) public view returns (address) {
+        address predictedAddress = address(uint160(uint(keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this), 
+                salt, 
+                keccak256(bytecode) 
+            )
+        ))));
+      
+        return predictedAddress;
+    }
+
+
+    /**
+     * @notice  . A function to create Contract using create2
+     * @dev     . returns address of the new contract. revert if called with the same parameter more than once
+     * @param   salt  . A random bytes used to precompute an address
+     * @param   bytecode  .byte code of the contract to be deployed
+    */
+    function createContract(bytes32 salt, bytes memory bytecode) public returns(address){
+        address contractAddress;
+        assembly {
+            contractAddress := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(contractAddress)) {
+                revert(0, 0)
+            }
+        }
+
+        emit NewContract(contractAddress);
+        return contractAddress;
+    }
+
+     /**
+     * @notice  . An helper function to get the bytes32 value of a number
+     * @dev     . returns bytes32
+     * @param   _salt  . A random uint value
+    */
+    function generateBytes(uint _salt) external pure returns(bytes32){
+        bytes32 salt = bytes32(_salt);
+        return salt;
+        
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+
+type HttpNetworkAccountsUserConfig = any;
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY] as HttpNetworkAccountsUserConfig | undefined,
+      chainId: 44787,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
+
+export default config;
